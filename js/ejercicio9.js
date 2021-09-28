ca489a19ea85f878cf6b9b6884cff21d3e13971a
alanChibilisco <alan.chibilisco@gmail.com>
2021-09-28 20:03:27 -0300

    se completo el ejercicio 9

--- js/ejercicio9.js
+++ js/ejercicio9.js
@@ -1,4 +1,19 @@
-document.write("<br>Ejercico 9");
+document.write("<br>Ejercico 9<br>");
 
 let str=prompt("Ingrese una frase");
+let vocal="";
 
+for(let i=0; i<str.length; i++){
+    vocal=str.substr(i,1);
+    if(vocal==="a"){
+        document.write(" "+vocal);
+    }else if(vocal==="e"){
+        document.write(" "+vocal);
+    }else if(vocal==="i"){
+        document.write(" "+vocal);
+    }else if(vocal==="o"){
+        document.write(" "+vocal);
+    }else if(vocal==="u"){
+        document.write(" "+vocal);
+    }
+}
